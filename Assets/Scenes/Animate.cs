c1b981d738eae2e3596aef083bd6ce781908fe5b
KyraSY <kyip123@terpmail.umd.edu>
2025-02-11 18:12:39 -0500

    Update Animate.cs

--- Assets/Scenes/Animate.cs
+++ Assets/Scenes/Animate.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 public class Animate : MonoBehaviour
 {
     GameObject[] spheres;
-    static int numSphere = 100; 
+    static int numSphere = 50;
     float time = 0f;
     Vector3[] initPos;
     // Start is called before the first frame update
@@ -17,8 +17,9 @@ public class Animate : MonoBehaviour
     {
         spheres = new GameObject[numSphere];
         initPos = new Vector3[numSphere];
-        
-        foreach (GameObject sphere in spheres){
+
+        foreach (GameObject sphere in spheres)
+        {
             // sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             // This will cause an error. Why?
             // foreach is a read only iterator that iterates dynamically classes that implement IEnumerable, each cycle in foreach will call the IEnumerable to get the next item, the item you have is a read only reference,
@@ -53,16 +54,20 @@ public class Animate : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
-        // what to update?
-        for (int i =0; i < numSphere; i++){
-            // position
-            spheres[i].transform.position = initPos[i] 
-                                            + new Vector3(0f, 0f, 3f * Mathf.Sin(time)) ;
-            // color
+        for (int i = 0; i < numSphere; i++)
+        {
+            spheres[i].transform.position = initPos[i] + new Vector3(0f, 0f, 3f * Mathf.Sin(time));
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
-            float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), Mathf.Cos(time), 2f + Mathf.Cos(time)); // Full saturation and brightness
+            float hue = (float)i / numSphere;
+            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), Mathf.Cos(time), 2f + Mathf.Cos(time));
             sphereRenderer.material.color = color;
+
+
+            float size = Mathf.Abs(Mathf.Sin(time + i * 0.2f)) * 0.5f + 0.5f;
+            spheres[i].transform.localScale = new Vector3(size, size, size);
+
+            spheres[i].transform.Rotate(new Vector3(0f, 30f * Time.deltaTime, 0f));
         }
     }
+
 }
